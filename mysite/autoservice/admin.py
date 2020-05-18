72d1af343b6a718eb5c65883bbb015f0bc79e3c0
Donatas Noreika <donoras@gmail.com>
2020-05-18 18:08:30 +0300

    3 Pridėtas automobilio admin list_view

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -10,9 +10,12 @@ class UzsakymasAdmin(admin.ModelAdmin):
     list_display = ('automobilis_id', 'suma')
     inlines = [UzsakymoEiluteInLine]
 
+class AutomobilisAdmin(admin.ModelAdmin):
+    list_display = ('klientas', 'automobilis_id', 'valstybinis_numeris', 'vin_kodas')
+
 admin.site.register(Paslauga)
 admin.site.register(Paslaugos_kaina)
 admin.site.register(AutomobilioModelis)
-admin.site.register(Automobilis)
+admin.site.register(Automobilis, AutomobilisAdmin)
 admin.site.register(Uzsakymas, UzsakymasAdmin)
 admin.site.register(UzsakymoEilute)
